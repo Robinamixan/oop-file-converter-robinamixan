39ed30d8b4efe9df92b84b1e91a2a31125d93e0f
robinam <robinamixan@mail.ru>
2017-12-07 15:59:54 +0300

    Fix Syntax 2

--- src/CSV/CSVSaver.php
+++ src/CSV/CSVSaver.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: robinam
- * Date: 06.12.17
- * Time: 15:35
- */
 
 namespace FileConverter\CSV;
 
@@ -13,7 +7,6 @@ class CSVSaver
 {
     public static function save(array $contain, string $outputFilePath): void
     {
-        var_dump($contain);
         $file = fopen($outputFilePath, 'w');
         foreach($contain as $row) {
             fputcsv($file, $row);
